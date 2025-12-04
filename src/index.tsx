ffa917bde69cb668da7a43ef22d5a55e2ba2c48b
Twiliness <729294652hs@gmail.com>
2025-12-04 10:36:29 +0800

    [Fix]: worker base path
    Signed-off-by: Twiliness <729294652hs@gmail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -49,7 +49,7 @@ window.addEventListener(
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("/sw.js")
+      .register(`${import.meta.env.BASE_URL}sw.js`)
       .then((registration) => {
         console.log("SW registered: ", registration);
       })
